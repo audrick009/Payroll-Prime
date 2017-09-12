b4191c969e4272222bec2af13220fab202e071c1
janaudrick009 <janaudrick.lazaro@benilde.edu.ph>
2017-09-12 21:59:12 +0800

    initial push
    first push

---
+++ Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection mio = new SqlConnection(Helper.GetCon());
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //string password = "";
+        //int UserID = 0;
+        //mio.Open();
+        //SqlCommand mirai = new SqlCommand();
+        //mirai.Connection = mio;
+        //mirai.CommandText = "Select Password, UserID from Users";
+        //SqlDataReader dr = mirai.ExecuteReader();
+        //if (dr.HasRows)
+        //{
+        //    while (dr.Read())
+        //    {
+        //        password = dr["Password"].ToString();
+        //        UserID = int.Parse(dr["UserID"].ToString());
+        //        changePass(password, UserID);
+        //    }
+        //}
+        //mio.Close();
+    }
+    void changePass(string pword, int userid)
+    {
+        SqlConnection rawr = new SqlConnection(Helper.GetCon());
+        rawr.Open();
+        SqlCommand aki = new SqlCommand();
+        aki.Connection = rawr;
+        aki.CommandText = "Update Users SET Password=@Password Where UserID = @UserID";
+        aki.Parameters.AddWithValue("@Password", Helper.CreateSHAHash(pword));
+        aki.Parameters.AddWithValue("@UserID", userid);
+        aki.ExecuteNonQuery();
+        rawr.Close();
+    }
+}
---
+++ DepartmentHead/getLeaveApplication.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class getLeaveApplication : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(Helper.GetCon());
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["userid"] != null)
+        {
+            if (Session["position"].ToString() == "Department Head")
+            {
+                GetLeaveApplication();
+            }
+            else
+            {
+                Response.Redirect("../Login.aspx");
+            }
+        }
+        else
+        {
+            Response.Redirect("../Login.aspx");
+        }
+    }
+
+
+    void GetLeaveApplication()
+    {
+        con.Open();
+        SqlCommand com = new SqlCommand();
+        com.Connection = con;
+        com.CommandText = "SELECT l.LeaveRID, e.Firstname + ' ' + e.Lastname as Fullname, l.Status, l.LeaveType, l.Days, l.StartingDate, l.EndingDate FROM LeaveRecords l INNER JOIN Employee e ON l.EmployeeID = e.EmployeeID WHERE l.Status = 'Pending'";
+        SqlDataReader dr = com.ExecuteReader();
+        lvLeaveApp.DataSource = dr;
+        lvLeaveApp.DataBind();
+        con.Close();
+        
+
+    }
+}
